3dbd88922a25e1c44afdcfe0da0e7dcf842ea215
chapati <chapati.th@gmail.com>
2020-07-06 23:16:11 +0300

    setting up build

--- service-balancer/version.go
+++ service-balancer/version.go
@@ -5,5 +5,5 @@ import "fmt"
 var balancerVersion string
 
 func printVersion () {
-	fmt.Println("Wallet Service Balancer ", balancerVersion)
+	fmt.Println("Wallet Service Balancer", balancerVersion)
 }
